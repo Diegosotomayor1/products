6508c094840e6a6069abf28e37d79423b291320d
Diego Sotomayor <diesor53@gmail.com>
2021-02-09 22:41:31 -0500

    Database Complete

--- database/db.sql
+++ database/db.sql
@@ -5,8 +5,8 @@ CREATE TABLE product(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     codigo VARCHAR(11) NOT NULL,
     producto VARCHAR(50) NOT NULL,
-    stock INT(6) NOT NULL,
-    precio FLOAT(11) NOT NULL
+    stock VARCHAR(6) NOT NULL,
+    compra FLOAT(11) NOT NULL
 
 );
 
